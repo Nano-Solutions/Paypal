5f98b30d69c597f0ae88681341d63dbf1256b50f
Adeyomi Akisanya <adeyomiakisanya@gmail.com>
2021-08-31 20:32:59 +0000

    newcommit

---
+++ demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This is a project executed on behalf of Paypal"
+
+echo "Enter your name"
+read name
+echo "Welcome $name"
+echo "Enter your password"
+read password
+echo "Login Successful !!!"
+
